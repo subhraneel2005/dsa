5db8d2112892a5832766b2dd7af59c0aedf44d04
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-11-02 18:48:50 +0530

    learned about Scanner

--- lecture-4/Main.java
+++ lecture-4/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args){
-        System.out.println("Kya re?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello" + " " + name);   
+
+        scanner.close();
     }
 }
