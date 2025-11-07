bb85edb044e428e595bc1da20c05c097e6d42890
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-11-07 17:47:08 +0530

    lecture-7/added loops in switch for running orders

--- lecture-7/Switch.java
+++ lecture-7/Switch.java
@@ -5,34 +5,47 @@ public class Switch {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("\n-----WELCOME TO MY LITTLE CAFE-----\n\nMENU\n1. Cold Coffee\n2. Macha Tea\n3. Chicken Sandwich\n4. CheeseCake\n5. Exit");
-        System.out.print("\nEnter your choice: ");
-        int choice = in.nextInt();
+        System.out.println("\n-----WELCOME TO MY LITTLE CAFE-----\n\nMENU\n1. Cold Coffee - Rs 120\n2. Macha Tea - Rs 220\n3. Chicken Sandwich - Rs 180\n4. CheeseCake - Rs 190\n5. Exit");
 
-        switch (choice) {
-            case 1 -> {
-                System.out.println("You selected Cold Coffee");
-                System.out.println("Price: Rs. 120");
+        int totalBill = 0;
+
+        while (true) {
+            System.out.print("\nEnter choice: ");
+            int choice = in.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("You selected Cold Coffee");
+                    totalBill += 120;
+                    System.out.println("Current bill: Rs. " + totalBill);
+                }
+
+                case 2 -> {
+                    System.out.println("You selected Macha Tea");
+                    totalBill += 220;
+                    System.out.println("Current bill: Rs. " + totalBill);
+                }
+                case 3 -> {
+                    System.out.println("You selected Chicken Sandwich");
+                    totalBill += 180;
+                    System.out.println("Current bill: Rs. " + totalBill);
+
+                }
+                case 4 -> {
+                    System.out.println("You selected CheeseCake");
+                    totalBill += 190;
+                    System.out.println("Current bill: Rs. " + totalBill);
+
+                }
+                case 5 -> {
+                    System.out.println("Your total bill is: Rs." + totalBill);
+                    System.out.println("Thank you for visiting! Have a great day!");
+                    System.exit(0);
+                }
+                default ->
+                    System.out.println("\nInalid Choice! Please select a valid option from the menu.\n");
             }
 
-            case 2 -> {
-                System.out.println("You selected Macha Tea");
-                System.out.println("Price: Rs. 220");
-            }
-            case 3 -> {
-                System.out.println("You selected Chicken Sandwich");
-                System.out.println("Price: Rs. 180");
-            }
-            case 4 -> {
-                System.out.println("You selected CheeseCake");
-                System.out.println("Price: Rs. 190");
-            }
-            case 5 -> {
-                System.out.println("Thank you for visiting! Have a great day!");
-                System.exit(0);
-            }
-            default ->
-                System.out.println("Inalid Choice! Please select a valid option from the menu.");
         }
+
     }
 }
