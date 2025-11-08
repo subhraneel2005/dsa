2f4c34c81aac4524be24fe144ac03aa778e60ffb
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-11-08 17:13:42 +0530

    lecture-8/static methods, return types and parameters

---
+++ lecture-8/Methods.java
@@ -0,0 +1,50 @@
+
+public class Methods {
+
+    public static void main(String[] args) {
+        divide();
+
+        int modulusResult = modulus(1234, 10);
+        System.out.println("Modulus result: " + modulusResult);
+
+        String greeting = greeting("KelaChuda");
+        System.out.println(greeting);
+
+        char grade = grade(93);
+        System.out.println("Grade: " + grade);
+    }
+
+    //method without return type and parameters
+    static void divide() {
+        int a, b, result;
+        a = 10;
+        b = 5;
+        result = a / b;
+        System.out.println("Division Result: " + result);
+    }
+
+    // method with retrn type ad parameters
+    static int modulus(int a, int b) {
+        return a % b;
+    }
+
+    // string return type method
+    static String greeting(String name) {
+        return "Hello " + name;
+    }
+
+    // character return type method
+    static char grade(int marks) {
+        if (marks >= 90 && marks <= 100) {
+            return 'A';
+        } else if (marks >= 80 && marks < 90) {
+            return 'B';
+        } else if (marks >= 70 && marks < 80) {
+            return 'C';
+        } else if (marks >= 60 && marks < 70) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
