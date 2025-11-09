adc3d70cff5b04ca92c666b09d30fc25fb59fd4b
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-11-09 18:49:23 +0530

    lecture-10/Arrays with primitive and complex dtypes

---
+++ lecture-10/Array.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+
+    public static void main(String[] args) {
+        // primitive();
+        complex();
+    }
+
+    static void primitive() {
+        int[] ids = new int[3];
+
+        Scanner inp = new Scanner(System.in);
+        for (int i = 0; i < ids.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            ids[i] = inp.nextInt();
+        }
+
+        System.out.println("Final array " + Arrays.toString(ids));
+    }
+
+    static void complex() {
+        String[] names = new String[3];
+        Scanner inp = new Scanner(System.in);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Enter name " + (i + 1) + ":");
+            names[i] = inp.nextLine();
+        }
+
+        System.out.println("Final Array: " + Arrays.toString(names));
+    }
+}
