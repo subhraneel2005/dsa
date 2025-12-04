1d0fe9f907921305f757ca177897d8e0a50b7245
subhraneel2005 <digitalsubhraneel@gmail.com>
2025-12-04 18:28:05 +0530

    arrays/Binary search first problem statement

---
+++ problem-statements/arrays/binary-search/First.java
@@ -0,0 +1,30 @@
+
+public class First {
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 5, 6, 7, 8, 9, 22, 33, 42, 55, 67, 88};
+        int target = 5;
+
+        int res = binarySearch(arr, target);
+        System.out.println(res);
+    }
+
+    static int binarySearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
