e75e54734c2c09de45e2069deb0f5642a0704e8e
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-01-14 12:53:35 +0530

    fix: wrote the L5 question with better optimization

--- lecture-17/L5.java
+++ lecture-17/L5.java
@@ -3,25 +3,8 @@
 public class L5 {
     public static void main(String[] args) {
         int arr[] = { 1, 3, 4, 2, 2 };
-        cycleSort(arr);
         int duplicate = findDuplicate(arr);
         System.out.println("duplicate element: " + duplicate);
-    }
-
-    static void cycleSort(int[] arr) {
-        // 1-n
-        int i = 0;
-
-        while (i < arr.length) {
-            int correct = arr[i] - 1;
-
-            if (arr[i] != arr[correct]) {
-                // swap i and correct
-                swap(arr, i, correct);
-            } else {
-                i++;
-            }
-        }
     }
 
     static void swap(int[] arr, int first, int second) {
@@ -32,9 +15,20 @@ public class L5 {
 
     static int findDuplicate(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) {
+        int i = 0;
+
+        while (i < arr.length) {
+
             if (arr[i] != i + 1) {
-                return arr[i];
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct]) {
+                    // swap i and correct
+                    swap(arr, i, correct);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
             }
         }
 
